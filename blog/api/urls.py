56ef9bd7aa38cd8137cee45a9ba5899213570f3a
Elegantovich <kh.msm@yandex.ru>
2022-05-09 18:21:09 +0300

    create View, serializers, endpoint for login

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import RecieveToken, PostViewSet, BlogViewSet
 
 router = DefaultRouter()
-router.register('posts', PostViewSet)
+router.register('blogs', BlogViewSet)
 
 urlpatterns = [
+    path('auth/token/login/', RecieveToken.as_view()),
+    # path('news/', .as_view()),
+    path('', include(router.urls))
 ]
