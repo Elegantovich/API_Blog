df650f10637cad29257484bb69ac965352ca2129
Elegantovich <kh.msm@yandex.ru>
2022-05-09 18:07:35 +0300

    create serializers for post

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from api.serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'pk'
